bce860fea395012e9967bd6251f706145b8b3362
eddygrinder <1210171@isep.ipp.pt>
2023-11-11 00:33:56 +0000

    update shift register

--- shift_register.py
+++ shift_register.py
@@ -6,16 +6,21 @@ GPIO.setmode(GPIO.BCM)
 import serial
 import time
 
+import warnings
+warnings.filterwarnings("ignore")
+
 SER = 5         # GPIO 5 - SER/DS (serial data input, SPI data)
 RCLK = 6        # GPIO 6 - RCLK/STCP
 SRCLK = 13      # GPIO 13 - SRCLK/SHCP (storage register clock pin, SPI clock)
 SRCLR = 26      # GPIO 26 - O registo de deslocamento é limpo (ACTIVO BAIXO)
+OE = 22         # GPIO 22 - Enable/Disable do SR
 
 # Setup dos pinos
 GPIO.setup(SER, GPIO.OUT)               
 GPIO.setup(RCLK, GPIO.OUT)                     
 GPIO.setup(SRCLK, GPIO.OUT)
 GPIO.setup(SRCLR, GPIO.OUT)
+GPIO.setup(OE, GPIO.OUT)
 
 # Inicializar a variavel correspondente a R1
 # Reles 1 e 2
@@ -24,17 +29,22 @@ GPIO.setup(SRCLR, GPIO.OUT)
 # Valor por defeito de espera nas operações do registo de deslocamento
 WaitTimeSR = 0.1
 
+#####################################################
+# Tabela de verdade do Registo de Deslocamento
+# SER | SRCLK | 'SRCLR | RCLK |  'OE | Saídas/Funções
+#  X      X       X       X       H    Q's inactivas
+#  X      X       X       X       L    Q'S activos
+#  X      X       L       X       X    SR limpo
+#  L    + et      H       X       X    0 no SR
+#  H    + et      H       X       X    1 no SR
+#  X      X       X     +et       X   dados out
+######################################################
+
 # Inicaializa o pino de clear dos registos a 1 - o clear é controlado e feito numa função
 GPIO.output(SRCLR,1)
 
-import warnings
-warnings.filterwarnings("ignore")
-
-# Funcao que limpa o registo
-def register_clear ():
-    GPIO.output(SRCLR, 0) # É feito o clear
-    time.sleep(WaitTimeSR) # espera 100ms
-    GPIO.output(SRCLR, 1)
+# Enable do SR - saídas sempre activas
+GPIO.output(OE, 0)
 
 # Função que verifica e desloca os bits para armazenar no registo de deslocamento
 def SRoutput(checkshift):
@@ -60,15 +70,18 @@ def SRoutput(checkshift):
 
 def WriteReg (WriteBit, WaitTimeSR):
     GPIO.output (SRCLK, 0)  # Clock - flanco POSITIVO
-    GPIO.output(RCLK,0) # Garantir que o LATCH está a zero antes de enviar o bit
     GPIO.output (SER,WriteBit) # Envia o bit para o registo
     time.sleep (WaitTimeSR) # Espera 100ms
     GPIO.output(SRCLK,1)
-    GPIO.output(SRCLK,0)
-    
+
+# Funcao que limpa o registo
+def register_clear ():
+    GPIO.output(SRCLK, 0)
+    time.sleep(WaitTimeSR) # espera 100ms
+    GPIO.output(SRCLK, 1)
+
 # Armazenar o valor no registo
 def OutputReg():
+    GPIO.output(RCLK, 0)
+    time.sleep(WaitTimeSR)
     GPIO.output(RCLK, 1)
-    #GPIO.cleanup()
-    while True:
-        pass
