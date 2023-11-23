b874de0d61e06120022fc9dc1bf627314851665b
eddygrinder <1210171@isep.ipp.pt>
2023-11-23 12:26:07 +0000

    update organized classes

--- Menu/menu_gui.py
+++ Menu/menu_gui.py
@@ -13,6 +13,84 @@ import threading
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
+class OhmWindow(customtkinter.CTk):
+    def __init__(self, title, values):
+        super().__init__()
+
+        self.geometry("300x200")
+        self.title("Lei Ohm")
+
+        self.checkbox_frame = CheckboxFrame(self, "Escolha", values=values)
+        self.checkbox_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsew")
+
+        self.button = customtkinter.CTkButton(self, text="OK", command=self.button_callback)
+        self.button.grid(row=1, column=0, padx=10, pady=10)
+
+        self.button_cancel = customtkinter.CTkButton(self, text="Cancelar", command=self.destroy)
+        self.button_cancel.grid(row=1, column=1, padx=10, pady=10)
+    
+    def button_callback(self):
+        checked_checkboxes = self.checkbox_frame.get()
+        if len(checked_checkboxes) != 1:
+            messagebox.showerror("Erro", "Selecione apenas uma opção.")
+        else:
+            option = checked_checkboxes[0]  # Obtém a opção selecionada
+        if option == "1K":
+           self.option_output(option)
+        elif option == "Lei de Kirchhoff":
+            new_window_Kirchhoff = KirchhoffWindow("Mais opções - Lei de Kirchhoff", values=["10K", "20K", "30K"])
+            new_window_Kirchhoff.mainloop()
+
+    def option_output(self, option):
+            self.draw_scheme(option)
+            caminho_imagem = "esquemaOhm.jpg"
+            subprocess.Popen([visualizador, caminho_imagem], shell=True)
+            #subprocess.run(["python", "Menu//scheme.py"]) 
+            print("OK")
+    
+    def draw_scheme(self, option):
+        with schemdraw.Drawing(show=False) as d:
+            d.config(unit=5) #tamanho do componente
+            # Adicionando os elementos
+            d += (V1 := elm.SourceV().label('Vin'))
+            d += (I1 := elm.MeterA().right().label('Amp'))
+            d += elm.Dot()
+            d.push()
+            d += (R1 := elm.Resistor().down().label(option, loc='bot'))
+            d += elm.Dot()
+            d.pop()
+            d += (L1 := elm.Line())
+            d += (Vol := elm.MeterV().down().label('Volt', loc='bot', rotate=True))
+            d += (L2 := elm.Line().tox(V1.start))            
+            #d.draw()
+            d.save('esquemaOhm.jpg')
+            
+class KirchhoffWindow(customtkinter.CTk):
+    def __init__(self, title, values):
+        super().__init__()
+
+        self.geometry("300x200")
+        self.title("Lei de Kirchhoff")
+        self.checkbox_frame = CheckboxFrame(self, "Escolha", values=values)
+        self.checkbox_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsew")        
+        
+        self.button = customtkinter.CTkButton(self, text="OK", command=self.button_callback)
+        self.button.grid(row=1, column=0, padx=10, pady=10)
+        
+        self.button_cancel = customtkinter.CTkButton(self, text="Cancelar", command=self.destroy)
+        self.button_cancel.grid(row=1, column=1, padx=10, pady=10)
+
+    def button_callback(self):
+        checked_checkboxes = self.checkbox_frame.get()
+        if len(checked_checkboxes) != 1:
+            messagebox.showerror("Erro", "Selecione apenas uma opção.")
+        else:
+            option = checked_checkboxes[0]  # Obtém a opção selecionada
+        
+        if option == "Lei de Kirchhoff":
+            new_window_Kirchhoff = KirchhoffWindow("Mais opções - Lei de Kirchhoff", values=["10K", "20K", "30K"])
+            new_window_Kirchhoff.mainloop()
+
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
@@ -27,7 +105,7 @@ class App(customtkinter.CTk):
         self.checkbox_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsew")
 
         self.button = customtkinter.CTkButton(self, text="OK/Seguinte", command=self.button_callback)
-        self.button.grid(row=3, column=0, padx=10, pady=10, sticky="ew")
+        self.button.grid(row=1, column=0, padx=10, pady=10, sticky="ew")
         # Botão "Cancelar" que fecha a janela atual
         self.button_cancel = customtkinter.CTkButton(self, text="Cancelar", command=self.destroy)
         self.button_cancel.grid(row=1, column=1, padx=10, pady=10)
@@ -39,31 +117,10 @@ class App(customtkinter.CTk):
         else:
             option = checked_checkboxes[0]  # Obtém a opção selecionada
         if option == "Lei de Ohm":
-            new_window_Ohm = customtkinter.CTk()
-            new_window_Ohm.geometry("300x200")
-            new_window_Ohm.title("Mais opções - Lei de Ohm")
-
-            self.checkbox_frame_Ohm = CheckboxFrame(new_window_Ohm, "Escolha a resistência", values=["1K", "2K", "3K"])
-            self.checkbox_frame_Ohm.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsew")
-
-            self.button_Ohm = customtkinter.CTkButton(new_window_Ohm, text="OK", command=self.button_second_window_Ohm)
-            self.button_Ohm.grid(row=1, column=0, padx=10, pady=10)
-            
-            # Botão "Cancelar" que fecha a janela atual
-            self.button_cancel = customtkinter.CTkButton(new_window_Ohm, text="Cancelar", command=new_window_Ohm.destroy)
-            self.button_cancel.grid(row=1, column=1, padx=10, pady=10)
+            new_window_Ohm = OhmWindow("Mais opções - Lei de Ohm", values=["1K", "2K", "3K"])
             new_window_Ohm.mainloop()
         elif option == "Lei de Kirchhoff":
-            new_window_Kirchhoff = customtkinter.CTk()
-            new_window_Kirchhoff.geometry("300x200")
-            new_window_Kirchhoff.title("Mais opções - Lei de Kirchhoff")
-
-            more_checkboxes_Kirchhoff = CheckboxFrame(new_window_Kirchhoff, "Escolha as resistências", values=["1K", "2K", "3K"])
-            more_checkboxes_Kirchhoff.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsew")
-
-            button_Kirchhoff = customtkinter.CTkButton(new_window_Kirchhoff, text="OK", command=self.button_second_window_Kirchhoff)
-            button_Kirchhoff.grid(row=1, column=0, padx=10, pady=10)
-
+            new_window_Kirchhoff = KirchhoffWindow("Mais opções - Lei de Kirchhoff", values=["10K", "20K", "30K"])
             new_window_Kirchhoff.mainloop()
         elif option == "DíodoTransistor":
             new_window_DíodoTransistor = customtkinter.CTk()
@@ -126,21 +183,6 @@ class CheckboxFrame(customtkinter.CTkFrame):
         checked_checkboxes = [checkbox.cget("text") for checkbox in self.checkboxes if checkbox.get() == 1]
         return checked_checkboxes
 
-def draw_scheme(option):
-    with schemdraw.Drawing(show=False) as d:
-        d.config(unit=5) #tamanho do componente
-        # Adicionando os elementos
-        d += (V1 := elm.SourceV().label('Vin'))
-        d += (I1 := elm.MeterA().right().label('Amp'))
-        d += elm.Dot()
-        d.push()
-        d += (R1 := elm.Resistor().down().label(option, loc='bot'))
-        d += elm.Dot()
-        d.pop()
-        d += (L1 := elm.Line())
-        d += (Vol := elm.MeterV().down().label('Volt', loc='bot', rotate=True))
-        d += (L2 := elm.Line().tox(V1.start))            
-        #d.draw()
-        d.save('esquemaOhm.jpg')
+
 app = App()
 app.mainloop()
