4a1c3a613ef8b58212d791bde6fa3fa8e829cf55
eddygrinder <1210171@isep.ipp.pt>
2023-11-30 22:40:41 +0000

    update

--- Menu/sub_process.py
+++ Menu/sub_process.py
@@ -10,7 +10,7 @@ def main:
     caminho_imagem = "esquemaOhm.png"
 
     # Inicia o subprocesso para abrir a imagem
-    subprocess.Popen([visualizador, caminho_imagem])
+    subprocess.Popen([visualisador, caminho_imagem])
 
     # O programa continuará a execução a partir daqui
     print("O programa continuou a execução após abrir a imagem.")
