9d8da3176514f2e47c1ab27aa389bef8ec9a84f6
eddygrinder <1210171@isep.ipp.pt>
2024-02-22 16:57:25 +0000

    passagem de parametros html not working

--- ctrl_hardware/controlVB.py
+++ ctrl_hardware/controlVB.py
@@ -25,7 +25,7 @@
 # THE SOFTWARE.
 
 import os, sys, requests
-#from pyvirtualbench import PyVirtualBench, PyVirtualBenchException, DmmFunction
+from pyvirtualbench import PyVirtualBench, PyVirtualBenchException, DmmFunction
 
 # Caminho para o diretório ctrl_hardware
 ctrl_hardware_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'ctrl_hardware'))
@@ -35,7 +35,7 @@ sys.path.append(ctrl_hardware_path)
 # You will probably need to replace "myVirtualBench" with the name of your device.
 # By default, the device name is the model number and serial number separated by a hyphen; e.g., "VB8012-309738A".
 # You can see the device's name in the VirtualBench Application under File->About
-#virtualbench = PyVirtualBench('VB8012-30A210F')
+virtualbench = PyVirtualBench('VB8012-30A210F')
 
 #from shift_register import SRoutput
 
@@ -49,17 +49,17 @@ def read_Vcc_R (Vcc, Resistence):
     voltage_level = Vcc
     current_limit = 0.5
 
-    #ps = virtualbench.acquire_power_supply()
+    ps = virtualbench.acquire_power_supply()
 
-    #ps.configure_voltage_output(channel, voltage_level, current_limit)
-    #ps.enable_all_outputs(True)
+    ps.configure_voltage_output(channel, voltage_level, current_limit)
+    ps.enable_all_outputs(True)
 
-    #dmm = virtualbench.acquire_digital_multimeter();
-    #dmm.configure_measurement(DmmFunction.DC_VOLTS, True, 10.0)
+    dmm = virtualbench.acquire_digital_multimeter();
+    dmm.configure_measurement(DmmFunction.DC_VOLTS, True, 10.0)
 
-    measurement_result = 1.0089654
-    print("Measurement: %f V" % (measurement_result))
-    print("Measurement: %f KOhm" % (Resistence))
+    measurement_result = dmm.read()
+    print("MeasurementV: %f V" % (measurement_result))
+    print("MeasurementR: %f KOhm" % (Resistence))
         # Atribui o valor à variável, garantindo o tipo correto
     #measurement_result = float(measurement_result)
     
@@ -72,8 +72,8 @@ def read_Vcc_R (Vcc, Resistence):
     resposta = requests.get(url)
     print(resposta.text)
     """
-    #dmm.release()
-    #ps.release()
+    dmm.release()
+    ps.release()
 
     # Construa a URL com o valor da medição
     
--- webserver/website/views.py
+++ webserver/website/views.py
@@ -7,34 +7,47 @@ from controlVB import read_Vcc_R
 
 views = Blueprint('views', __name__)
 
+# Esta rota será acessada através de AJAX para obter o valor atualizado da medição
+@views.route('/obter-valor-da-medicao')
+def obter_valor_da_medicao():
+    # Aqui você pode definir o valor da medição que deseja passar para o HTML
+    measurement_results = "valor_da_medicao"
+    return jsonify({'medicao': measurement_results})
 
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    return render_template("home.html", user=current_user)
+    try:
+        if request.method == 'GET':
+        # Renderiza o HTML inicial
+            return render_template("your_template.html")
+        elif request.method == 'POST':
+            Vcc = request.args.get('Vcc', 0, int)
+            Resistence = request.args.get('R',0, int)
+            print(f'Valores Recebidos - Vcc: {Vcc}, Resitence: {Resistence}')
+            # Chamar a função que estará definida no script control_VB e passar os dois parâmetros recebids
+            # Atenção Vcc string e Resistence string
+            #Vcc_float = float(Vcc) # Atribui o valor à variável, garantindo o tipo correto
+            # Verificar se ambos os valores são diferentes de zero
+            if Vcc != 0 and Resistence != 0:
+            # Chamar a função apenas se os valores não forem zero
+                measurement_results = read_Vcc_R(Vcc, Resistence)
+                print(f'MeAsure: {measurement_results}')
+            else:
+                #  Se um ou ambos os valores forem zero, defina measurement_results como zero
+                measurement_results = 0
+
+            # Renderize o template 'home.html' com os resultados da medição
+            return render_template("home.html", user=current_user, measurement_result=measurement_results)
+    except Exception as e:
+        # Lidar com possíveis erros e mostrar uma mensagem de erro
+        measurement_error = str(e)
+        return render_template("home.html", user=current_user, measurement_error=measurement_error)
 
 #########################################################
 # Rota para passar parâmetros para o script controlVB.py
 # Só passa os parâmetros de escolha    
 #########################################################
-@views.route('/control_virtual_bench', methods=['GET'])
-@login_required
-def control_virtual_bench():
-    try:
-        Vcc = request.args.get('Vcc','')
-        Resistence = request.args.get('R','')
-        print(f'Valores Recebidos - Vcc: {Vcc}, Resitence: {Resistence}')
-        # Chamar a função que estará definida no script control_VB e passar os dois parâmetros recebids
-        
-        measurement_result = 1.2
-        print(f'Measure: {measurement_result}')
-
-        # Renderize o template 'home.html' com os resultados da medição
-        return render_template("home.html", user=current_user, measurement_result = measurement_result)
-    except Exception as e:
-        # Lidar com possíveis erros e mostrar uma mensagem de erro
-        measurement_error = str(e)
-        return render_template("home.html", user=current_user, measurement_error=measurement_error)
 
 
 @views.route('/delete-note', methods=['POST'])
